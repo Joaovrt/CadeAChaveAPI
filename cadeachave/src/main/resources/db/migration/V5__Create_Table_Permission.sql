1baa7d0b98756a077f8209fff1f2051d63886784
Joaovrt <joaovictorosa.tagliarini@gmail.com>
2024-03-21 22:00:34 -0300

    MigraçõesArrumadas

--- cadeachave/src/main/resources/db/migration/V5__Create_Table_Permission.sql
+++ cadeachave/src/main/resources/db/migration/V5__Create_Table_Permission.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE TABLE IF NOT EXISTS permission (
+﻿CREATE TABLE IF NOT EXISTS permission (
   id SERIAL NOT NULL,
   description varchar(255) NOT NULL,
   PRIMARY KEY (id)
--- cadeachave/src/main/resources/db/migration/V7__Create_Table_Users.sql
+++ cadeachave/src/main/resources/db/migration/V7__Create_Table_Users.sql
@@ -2,10 +2,10 @@
   id SERIAL NOT NULL,
   user_name varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
-  account_non_expired boolean(1) DEFAULT NULL,
-  account_non_locked boolean(1) DEFAULT NULL,
-  credentials_non_expired boolean(1) DEFAULT NULL,
-  enabled boolean(1) DEFAULT NULL,
+  account_non_expired boolean DEFAULT NULL,
+  account_non_locked boolean DEFAULT NULL,
+  credentials_non_expired boolean DEFAULT NULL,
+  enabled boolean DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT uk_user_name UNIQUE (user_name)
 ) ;
